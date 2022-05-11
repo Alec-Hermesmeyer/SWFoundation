0ce7f5e419292d4b2e23eb0facf7f9f81dd458ad
Alec-Hermesmeyer <ahrmsmyr@gmail.com>
2022-05-11 16:01:33 -0500

    first commit

---
+++ public/spare.jsx
@@ -0,0 +1,18 @@
+<section className="home-intro">
+        <div className="left-home">
+          <h1>Commercial and Limited-Access Pier Drilling</h1>
+          <h3>S&W Foundation Contractors is the most reliable choice for all foundation drilling</h3>
+          <button className='estimate-button'>Get An Estimate</button>
+        </div>
+        <div className="center-home">
+          <img className='center-home-img' src={drill} alt="Rig drilling hole" />
+        </div>
+        <div className="right-home">
+          <div className="img-container-home-right">
+            <div className="top-container">
+              <div className="top-title">
+               </div>
+            </div>
+          </div>
+        </div>
+      </section>
